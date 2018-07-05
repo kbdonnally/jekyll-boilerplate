e6f455e47d06c960ccc1bc5fde0a2ba4fd142332
Katherine Donnally <kbd3ax@virginia.edu>
2018-07-05 16:37:22 -0400

    Add gulpfile copied from SLab css-gulp branch
    - Not quite done but it's a start!
    
    - Will need to install all the packages 'n' stuff in a sec

---
+++ gulpfile.js
@@ -0,0 +1,60 @@
+// gulpfile.js
+
+// require modules:
+var gulp   = require('gulp'),
+	clean  = require('gulp-clean'),
+	concat = require('gulp-concat'),
+	uglify = require('gulp-uglify-es').default,
+	insert = require('gulp-insert');
+
+var js_format = () => {
+	return gulp.src('./assets/js/_partials/*.js')
+		.pipe(concat('main.js'))
+		.pipe(uglify({ keep_fnames: true }))
+		.pipe(insert.wrap('(function() {', '\n})();'))
+		.pipe(gulp.dest('./assets/js'));
+}
+
+gulp.task('js_watch', () => {
+	return gulp.watch('./assets/js/_partials/*.js', js_format);
+});
+
+gulp.task('js_clean', () => {
+	return gulp.src('./assets/js/main.js', { allowEmpty: true })
+		.pipe(clean());
+});
+
+gulp.task('js', gulp.series('js_clean', 'js_watch'));
+
+// CSS:
+
+var sass = require('gulp-sass'),
+	autoprefixer = require('gulp-autoprefixer'),
+	replace = require('gulp-replace');
+
+var css_format = () => {
+	return gulp.src('./assets/css/style.scss')
+		.pipe(replace("---", "/* remove Jekyll frontmatter */"))
+		.pipe(sass({
+            includePaths: ['./assets/css/_sass'],
+            outputStyle: 'compressed'
+        }).on('error', sass.logError))
+        .pipe(autoprefixer({
+        	browsers: ['last 2 versions'],
+        	grid: true
+        }))
+		.pipe(gulp.dest('./assets/css'))
+		.pipe(gulp.dest('./_site/assets/css'));
+
+};
+
+gulp.task('css_watch', () => {
+	return gulp.watch('./assets/css/_sass/*.scss', css_format);
+});
+
+gulp.task('css_clean', () => {
+	return gulp.src('./assets/css/test/style.?(s)css', { allowEmpty: true })
+		.pipe(clean());
+});
+
+gulp.task('css', gulp.series('css_clean', 'css_watch'));
