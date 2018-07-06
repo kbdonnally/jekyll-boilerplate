a92f17bddb62125da0f82e3e7051f4485fafc4a7
Katherine Donnally <kbd3ax@virginia.edu>
2018-07-06 17:35:35 -0400

    Refactor tasks & combine into 1 'assets' task
    - See docs/gulp-workflow.md for a printout of the task structure
    
    - They're pretty specific so the tree alone should be at least kinda
      helpful for what's going on

--- gulpfile.js
+++ gulpfile.js
@@ -19,37 +19,82 @@ var gulp 		 = require('gulp'),
 // 				 Tasks: JavaScript
 // --------------------------------------------------
 
-// concatenate + minify + wrap in IIFE
-var js_main_format = () => {
-	return gulp.src('_assets/js/_partials/*.js')
+// ------ format JS: --------------------------------
+// ------ concatenate + minify + wrap in IIFE -------
+
+// define using functions
+var js_main_format = (done) => {
+	gulp.src('_assets/js/_partials/*.js')
 		.pipe(concat('main.js'))
 		.pipe(uglify({ keep_fnames: true }))
 		.pipe(insert.wrap('(function() {', '\n})();'))
 		.pipe(gulp.dest('_site/assets/js'))
 		.pipe(gulp.dest('assets/js'));
+	done();
 }
 
-// watch -> change -> format
+var js_lib_format = (done) => {
+	gulp.src('_assets/js/lib/*.min.js')
+		.pipe(gulp.dest('_site/assets/js/lib'))
+		.pipe(gulp.dest('assets/js/lib'));
+	done();
+};
+
+// make tasks for each
+gulp.task('js_main_format', js_main_format);
+gulp.task('js_lib_format', js_lib_format);
+
+// result:
+gulp.task('js_format', gulp.parallel('js_main_format', 'js_lib_format'));
+
+
+// ------ clean JS: ---------------------------------
+
+// delete compiled file
+var js_main_clean = (done) => {
+	gulp.src('assets/js/*.js', { allowEmpty: true })
+		.pipe(clean());
+	done();
+};
+
+var js_site_clean = (done) => {
+	gulp.src('_site/assets/js/*.js', { allowEmpty: true })
+		.pipe(clean());
+	done();
+};
+
+// tasks for each location
+gulp.task('js_main_clean', js_main_clean);
+gulp.task('js_site_clean', js_main_clean);
+
+// result:
+gulp.task('js_clean', gulp.parallel('js_main_clean', 'js_site_clean'));
+
+
+// ---------------- JS: Watching --------------------
+
+// rewrite if changes
 gulp.task('js_main_watch', () => {
 	return gulp.watch('_assets/js/_partials/*.js', js_main_format);
 });
 
-// delete compiled file -> start over
-gulp.task('js_main_clean', () => {
-	return gulp.src('assets/js/main.js', { allowEmpty: true })
-		.pipe(clean());
-});
+/* NB: lib doesn't change -> no cleaning or watching */
 
-// clear compiled JS -> rewrite whenever JS partial changes
-gulp.task('js_main', gulp.series('js_main_clean', 'js_main_watch'));
+// ---------------- JS: Final -----------------------
+
+// delete, reformat, rewrite if changes
+gulp.task('js', gulp.series('js_clean', 'js_format', 'js_main_watch'));
+
 
 // --------------------------------------------------
 // 					Tasks: CSS
 // --------------------------------------------------
 
+// --------------- CSS: Compiling -------------------
+
 // compile + minify + autoprefix
-var css_main_format = () => {
-	return gulp.src('_assets/css/style.scss')
+var css_main_format = (done) => {
+	gulp.src('_assets/css/style.scss')
 		.pipe(sass({
 			includePaths: ['./_assets/css/_sass'],
 			outputStyle: 'compressed'
@@ -59,22 +104,57 @@ var css_main_format = () => {
 			grid: true
 		}))
 		.pipe(gulp.dest('_site/assets/css'))
-		.pipe(gulp.dest('assets/css'))
+		.pipe(gulp.dest('assets/css'));
+	done();
 };
 
-// watch -> change -> format
+// pass along unchanged
+var css_lib_format = (done) => {
+	gulp.src('_assets/css/lib/*.css')
+		.pipe(gulp.dest('_site/assets/css/lib'))
+		.pipe(gulp.dest('assets/css/lib'));
+	done();
+};
+
+// make into tasks
+gulp.task('css_main_format', css_main_format);
+gulp.task('css_lib_format', css_lib_format);
+
+// result:
+gulp.task('css_format', gulp.parallel('css_main_format', 'css_lib_format'));
+
+// --------------- CSS: Cleaning --------------------
+
+// delete compiled file
+var css_main_clean = (done) => {
+	gulp.src('assets/css/style.css', { allowEmpty: true })
+		.pipe(clean());
+	done();
+};
+
+var css_site_clean = (done) => {
+	gulp.src('_site/assets/css/style.css', { allowEmpty: true })
+		.pipe(clean());
+	done();
+};
+
+// make into tasks
+gulp.task('css_main_clean', css_main_clean);
+gulp.task('css_site_clean', css_site_clean);
+
+// result:
+gulp.task('css_clean', gulp.parallel('css_main_clean', 'css_site_clean'));
+
+// ----------------- CSS: Watch ---------------------
+
 gulp.task('css_main_watch', () => {
 	return gulp.watch('_assets/css/**/*.scss', css_main_format);
 });
 
-// delete compiled file -> start over
-gulp.task('css_main_clean', () => {
-	return gulp.src('assets/css/style.css', { allowEmpty: true })
-		.pipe(clean());
-});
+// ----------------- CSS: Final ---------------------
 
-// clear compiled folder -> rewrite when SCSS partials change
-gulp.task('css_main', gulp.series('css_main_clean', 'css_main_watch'));
+gulp.task('css', gulp.series('css_clean', 'css_format', 'css_main_watch'));
+
 
 // --------------------------------------------------
 // 					Tasks: Images
@@ -88,22 +168,35 @@ var img_all_format = () => {
 		.pipe(gulp.dest('assets/img'));
 };
 
+gulp.task('img_format', img_all_format);
+
 // watch -> change -> send files
-gulp.task('img_all_watch', () => {
+gulp.task('img_watch', () => {
 	return gulp.watch(paths.imgSrc, img_all_format);
 });
 
 // delete folder the site reads
-gulp.task('img_all_clean', () => {
+gulp.task('img_clean', () => {
 	return gulp.src('assets/img', { allowEmpty: true })
 		.pipe(clean());
 });
+/* NB: not bothering to completely rewrite rn */
 
 // clear site-read folder -> rewrite when images change
-gulp.task('img_all', gulp.series('img_all_clean', 'img_all_watch'));
+gulp.task('img', gulp.series('img_clean', 'img_format', 'img_watch'));
 
 // --------------------------------------------------
-// 			  Tasks: Build main assets
+// 			  Tasks: Build & watch assets
 // --------------------------------------------------
 
-gulp.task('assets_main', gulp.parallel('js_main', 'css_main', 'img_all'));
+gulp.task('assets', gulp.parallel('js', 'css', 'img'));
+
+/* NB: add explanatory notes on a copy of this file &
+ * turn that into docs; delete most of the notes on the
+ * version you'll use in 'production'
+ *
+ * Eg: these are prob separated into sub-optimal # of tasks
+ * rn, but I'd prefer the code to be more readable than a couple
+ * milliseconds faster; in addition to being able to make more
+ * surgical changes to what the tasks do.
+ */
